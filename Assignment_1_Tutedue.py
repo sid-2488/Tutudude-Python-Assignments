3f2411c639607e99443073ace9c42cc3629ab296
sid-2488 <msiddharth33@gmail.com>
2026-01-26 15:54:45 +0530

    Added Python assignment files

---
+++ Assignment_1_Tutedue.py
@@ -0,0 +1,9 @@
+Num1 = input("Enter the first number: ")
+Num2 = input("Enter the second number: ")
+Num1 = int(Num1)
+Num2 = int(Num2)
+print("addition:", Num1 + Num2)
+print("subtraction:", Num1 - Num2)
+print("multiplication:", Num1 * Num2)
+print("division:", Num1 / Num2)
+
